789649b5e489cd77fb6972e6907683bcb210440e
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-12 15:17:02 +0300

    Semantic ui usage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 function Index({campaigns}) {
+
+    function renderCampaigns() {
+        const items = campaigns.map((item, i) => {
+            return {
+                header: item,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        })
+
+        return <Card.Group items={items} />;
+    }
     
     return (
-        <div>{campaigns}</div>
+        <div>
+            <link
+                async
+                rel="stylesheet"
+                href="//cdn.jsdelivr.net/npm/semantic-ui@$2.0.3/dist/semantic.min.css"
+            />
+            {renderCampaigns()}
+        </div>
     )
 }
 
