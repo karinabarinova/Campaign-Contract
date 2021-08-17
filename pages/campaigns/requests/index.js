647720baddab5b5f6f29b0d3e69d427564a7e09a
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-08-17 18:59:08 +0300

    Rendering a table, added request row component

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
-const RequestIndex = ({address}) => {
+const RequestIndex = ({address, requestsCount, requests, approversCount}) => {
+    const { Header, Row, HeaderCell, Body} = Table;
+
+    function renderRows() {
+        return requests.map((item, index) => {
+            return <RequestRow 
+                request={item}
+                key={index}
+                address={address}
+                id={index}
+                approversCount={approversCount}
+            />
+        })
+    }
+
     return (
         <Layout>
             <h3>Requests</h3>
@@ -13,6 +28,22 @@ const RequestIndex = ({address}) => {
                     <Button primary>Add Request</Button>
                 </a>
             </Link>
+            <Table>
+                <Header>
+                    <Row>
+                        <HeaderCell>ID</HeaderCell>
+                        <HeaderCell>Description</HeaderCell>
+                        <HeaderCell>Amount</HeaderCell>
+                        <HeaderCell>Recipient</HeaderCell>
+                        <HeaderCell>Approval Count</HeaderCell>
+                        <HeaderCell>Approve</HeaderCell>
+                        <HeaderCell>Finalize</HeaderCell>
+                    </Row>
+                </Header>
+                <Body>
+                    {renderRows()}
+                </Body>
+            </Table>
         </Layout>
     )
 }
@@ -20,6 +51,7 @@ const RequestIndex = ({address}) => {
 RequestIndex.getInitialProps = async (props) => {
     const {address} = props.query;
     const campaign = Campaign(address);
+    const approversCount = await campaign.methods.approversCount().call();
     const requestsCount = await campaign.methods.getRequestsCount().call();
     const requests = await Promise.all(
         Array(parseInt(requestsCount))
@@ -28,7 +60,7 @@ RequestIndex.getInitialProps = async (props) => {
                 return campaign.methods.requests(index).call()
             })
     );
-    return {address, requestsCount, requests};
+    return {address, requestsCount, requests, approversCount};
 }
 
 export default RequestIndex;
